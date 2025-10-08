39fd68e66761416d609d34eef65bd6312a440b2a
Umar Habib <mumarhabibrb102@gmail.com>
2025-10-08 13:19:10 +0500

    middleware issue

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'user-role' => \App\Http\Middleware\UserRoleMiddleWare::class,
-        'admin-role' => \App\Http\Middleware\AdminRoleMiddleWare::class,
+        'admin-role' => \App\Http\Middleware\AdminRoleMiddleware::class,
         'user-role' => \App\Http\Middleware\UserRoleMiddleWare::class,
     ];
 }
